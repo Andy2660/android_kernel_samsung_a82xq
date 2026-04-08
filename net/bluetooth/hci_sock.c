80b08dfe0e6d8905c658b0adeee675e160c9aff0
Andy2660 <shin.jun.hong0224@gmail.com>
2026-04-08 16:31:25 +0900

    net: bluetooth: fix error: use of undeclared identifier

--- net/bluetooth/hci_sock.c
+++ net/bluetooth/hci_sock.c
@@ -1065,7 +1065,7 @@ done:
 static int hci_sock_bind(struct socket *sock, struct sockaddr *addr,
 			 int addr_len)
 {
-    /*
+#if 0
 	struct sockaddr_hci haddr;
 	struct sock *sk = sock->sk;
 	struct hci_dev *hdev = NULL;
@@ -1357,7 +1357,7 @@ static int hci_sock_bind(struct socket *sock, struct sockaddr *addr,
 done:
 	release_sock(sk);
 	return err;
-    */
+#endif
     return 0;
 }
 
