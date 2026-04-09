bfb7886819104c1e517bfc87a49521af945da9de
Andy2660 <shin.jun.hong0224@gmail.com>
2026-04-09 12:24:51 +0900

    usb: gadget: rndis: add spinlock for rndis response list

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -941,7 +941,6 @@ struct rndis_params *rndis_register(void (*resp_avail)(void *v), void *v,
 	}
 #endif
 
-	spin_lock_init(&params->lock);
 	params->confignr = i;
 	params->used = 1;
 	params->state = RNDIS_UNINITIALIZED;
@@ -1136,7 +1135,6 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 			return r->buf;
 		}
 	}
-	spin_unlock_irqrestore(&params->lock, flags);
 
 	spin_unlock(&params->resp_lock);
 	return NULL;
